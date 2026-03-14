b3e8dcc5e6cdac00f1d05fc38b6fdd0f6f072be0
Rafael Brasilino <rafaelbrasilino20@gmail.com>
2026-03-14 19:05:05 -0300

    Atualizando arquivos do projeto

--- android/app/src/main/java/com/rafael/sololeveling/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.rafael.sololeveling;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "solo-leveling-cache-v23";
+const CACHE_NAME = "solo-leveling-cache-v24";
 const ASSETS = [
   "./",
   "./index.html",
